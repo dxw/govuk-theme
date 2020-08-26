8b3644c65e05ca9a394d8d46019b8e1e806132ee
Rob Skilling <rob@dxw.com>
2020-08-26 15:45:51 +0100

    Tidy mark-up for post meta

--- templates/partials/entry-meta.php
+++ templates/partials/entry-meta.php
@@ -1,8 +1,6 @@
-<ul class="entry-meta">
-    <li>
-        <time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
-    </li>
-    <li>
-        <span class="author"><?php echo __('By', 'roots'); ?> <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><?php echo get_the_author(); ?></a></span>
-    </li>
-</ul>
+<div class="govuk-body-s">
+    <span class="govuk-visually-hidden">Published by: </span>
+    <span class="author"><a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><?php echo get_the_author(); ?></a></span>,    
+    <span class="govuk-visually-hidden">on: </span>
+    <time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
+</div>
