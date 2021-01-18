0544fbd5555d1a0bcac4aeef90849a934d69cf71
Rob Skilling <rob@dxw.com>
2021-01-18 16:43:07 +0000

    Migrate tests from Peridot to Kahlan
    Kahlan is our standard unit testing framework now.
    
    This commit therefore:
    
    - Removes Peridot, Leo and the Peridot Dot Reporter as composer dependencies
    - Adds Kahlan as a composer dependency
    - Also removes WP_Mock as a dependency (as we can do its job in Kahlan)
    - Refactors the tests to run in Kahlan
    - Updates the CI file to run the new tests

--- spec/theme/widgets.spec.php
+++ spec/theme/widgets.spec.php
@@ -1,38 +1,35 @@
 <?php
 
+use \Kahlan\Arg;
+
 describe(\Dxw\GovukTheme\Theme\Widgets::class, function () {
     beforeEach(function () {
-        \WP_Mock::setUp();
         $this->widgets = new \Dxw\GovukTheme\Theme\Widgets();
     });
 
     afterEach(function () {
-        \WP_Mock::tearDown();
     });
 
     it('is registrable', function () {
-        expect($this->widgets)->to->be->an->instanceof(\Dxw\Iguana\Registerable::class);
+        expect($this->widgets)->toBeAnInstanceOf(\Dxw\Iguana\Registerable::class);
     });
 
     describe('->register()', function () {
         it('initialises the widgets', function () {
-            \WP_Mock::expectActionAdded('widgets_init', [$this->widgets, 'widgetsInit']);
+            allow('add_action')->toBeCalled();
+            expect('add_action')->toBeCalled()->once()->with('widgets_init', [$this->widgets, 'widgetsInit']);
             $this->widgets->register();
         });
     });
 
     describe('->widgetsInit()', function () {
         it('registers any widgets in the theme ', function () {
-            \WP_Mock::wpFunction('__', [
-                'return' => function ($a) {
-                    return $a;
-                }
-            ]);
+            allow('__')->toBeCalled()->andRun(function ($a) {
+                return $a;
+            });
 
-            \WP_Mock::wpFunction('register_sidebar', [
-                'args' => [\WP_Mock\Functions::type('array')],
-                'times' => 2,
-            ]);
+            allow('register_sidebar')->toBeCalled();
+            expect('register_sidebar')->toBeCalled()->times(2)->with(Arg::toBeAn('array'));
 
             $this->widgets->widgetsInit();
         });
--- spec/theme/wp_head.spec.php
+++ spec/theme/wp_head.spec.php
@@ -2,21 +2,20 @@
 
 describe(\Dxw\GovukTheme\Theme\WpHead::class, function () {
     beforeEach(function () {
-        \WP_Mock::setUp();
         $this->wpHead = new \Dxw\GovukTheme\Theme\WpHead();
     });
 
     afterEach(function () {
-        \WP_Mock::tearDown();
     });
 
     it('is registrable', function () {
-        expect($this->wpHead)->to->be->an->instanceof(\Dxw\Iguana\Registerable::class);
+        expect($this->wpHead)->toBeAnInstanceOf(\Dxw\Iguana\Registerable::class);
     });
 
     describe('->register()', function () {
         it('adds actions', function () {
-            \WP_Mock::expectActionAdded('init', [$this->wpHead, 'init']);
+            allow('add_action')->toBeCalled();
+            expect('add_action')->toBeCalled()->once()->with('init', [$this->wpHead, 'init']);
             $this->wpHead->register();
         });
     });
@@ -37,11 +36,21 @@ describe(\Dxw\GovukTheme\Theme\WpHead::class, function () {
                 ['wp_head', 'adjacent_posts_rel_link', 10, 0],
             ];
 
+            allow('remove_action')->toBeCalled();
+
             foreach ($actions as $args) {
-                \WP_Mock::wpFunction('remove_action', [
-                    'args' => $args,
-                    'times' => 1
-                ]);
+                if (count($args) == 2) {
+                    list($a, $b) = $args;
+                    expect('remove_action')->toBeCalled()->once()->with($a, $b);
+                }
+                if (count($args) == 3) {
+                    list($a, $b, $c) = $args;
+                    expect('remove_action')->toBeCalled()->once()->with($a, $b, $c);
+                }
+                if (count($args) == 4) {
+                    list($a, $b, $c, $d) = $args;
+                    expect('remove_action')->toBeCalled()->once()->with($a, $b, $c, $d);
+                }
             }
             $this->wpHead->init();
         });
