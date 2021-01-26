c1fa5c7ebb8fff6ce01cfef99fdbafbaf7a59072
Rob Skilling <rob@dxw.com>
2021-01-26 16:45:13 +0000

    Apply filters to all default govuk-prefixed classes
    Before: all govuk-prefixed classes were hardcoded into the template.
    
    Now: all govuk-prefixed classes are filtered by the `govuk_theme_class` filter. This means you could e.g. add an extra class of your own to all the govuk- prefixed elements, and use that to target the elements you wished to restyle via custom css in a child theme, or just filter specific govuk- class names to change them as you see fit. This may be preferable to having to re-write entire templates in your child theme.
    
    Resolves: #7

--- templates/partials/navigation.php
+++ templates/partials/navigation.php
@@ -1,12 +1,12 @@
 <nav class="header-navigation" role="navigation">
-    <button type="button" class="govuk-header__menu-button govuk-js-header-toggle" aria-controls="header-navigation" aria-label="Show or hide Top Level Navigation">Menu</button>
+    <button type="button" class="<?php echo apply_filters('govuk_theme_class', 'govuk-header__menu-button govuk-js-header-toggle') ?>" aria-controls="header-navigation" aria-label="Show or hide Top Level Navigation">Menu</button>
     <?php
     if (has_nav_menu('header')) {
         wp_nav_menu(array(
             'theme_location' => 'header',
             'container_class' => 'menu-header',
             'depth' => 1,
-            'menu_class' => 'govuk-header__navigation',
+            'menu_class' => apply_filters('govuk_theme_class', 'govuk-header__navigation'),
             'menu_id' => 'header-navigation',
             'container' => false
         ));
