fb97fd26a37e634dd3ad404dfa3a992e008397f3
Rob Skilling <rob@dxw.com>
2021-01-26 17:08:22 +0000

    Apply govuk-link class to all appropriate links
    We'd missed a few, which were consequently not styled properly.

--- templates/partials/entry-meta.php
+++ templates/partials/entry-meta.php
@@ -1,6 +1,6 @@
 <div class="<?php echo apply_filters('govuk_theme_class', 'govuk-body-s') ?>">
     <span class="<?php echo apply_filters('govuk_theme_class', 'govuk-visually-hidden') ?>">Published by: </span>
-    <span class="author"><a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><?php echo get_the_author(); ?></a></span>,    
+    <span class="author"><a class="<?php echo apply_filters('govuk_theme_class', 'govuk-link') ?>" href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author"><?php echo get_the_author(); ?></a></span>,    
     <span class="<?php echo apply_filters('govuk_theme_class', 'govuk-visually-hidden') ?>">on: </span>
     <time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
 </div>
