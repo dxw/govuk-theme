5f720b253e7356c37f9d06e3e4f3ccea013161a9
Rob Skilling <rob@dxw.com>
2021-03-15 16:20:49 +0000

    Use WordPress core pagination functions
    Before: We were using the dxw pagination class for pagination links. This is out of date, buggy and no longer supported.
    
    Now: We use the standard core pageinate_links() function, with govuk-type styling.
    
    Resolves: #14

--- templates/partials/pager.php
+++ templates/partials/pager.php
@@ -1,7 +1,11 @@
 <?php if ($wp_query->max_num_pages > 1) : ?>
 
-    <div class="pager">
-        <?php h()->pagination(); ?>
-    </div>
+    <nav class="<?php echo apply_filters('govuk_theme_class', 'govuk-pagination__container') ?>" role="pagination" aria-label="Pagination Navigation">
+        <?php echo paginate_links([
+            'type' => 'list',
+            'prev_next' => false,
+            'before_page_number' => '<span class="govuk-visually-hidden">Page </span>'
+        ]); ?>
+    </nav>
 
 <?php endif; ?>
