fcff37e8dee74ae12110fcf4b13128820c326c84
Calum Ryan <dev@calumryan.com>
2021-06-10 11:40:07 +0100

    Associate unlabelled search and add context

--- templates/partials/search-form.php
+++ templates/partials/search-form.php
@@ -1,8 +1,8 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url('/'); ?>">
 
-    <input type="search" value="<?php echo is_search() ? get_search_query() : '' ?>" name="s" class="search-field form-control" placeholder="<?php _e('Search', 'govuk-theme'); ?> <?php bloginfo('name'); ?>">
+    <input type="search" value="<?php echo is_search() ? get_search_query() : '' ?>" name="s" id="site-search" class="search-field form-control" placeholder="<?php _e('Search on ', 'govuk-theme'); ?> <?php bloginfo('name'); ?>">
 
-    <label class="hide"><?php _e('Search for:', 'govuk-theme'); ?></label>
+    <label class="govuk-visually-hidden" for="site-search"><?php _e('Search on ', 'govuk-theme'); ?> <?php bloginfo('name'); ?></label>
 
     <button type="submit" class="search-submit"><?php _e('Search', 'govuk-theme'); ?></button>
 </form>
