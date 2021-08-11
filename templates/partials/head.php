a9d366ae9cad1b9e55586960b444c9497fc6d883
Rob Skilling <rob@dxw.com>
2021-08-11 10:30:44 +0100

    Move <head> content to a partial
    This will allow us to over-ride the head content in a child theme
     without needing to over-ride the entire layout template.

---
+++ templates/partials/head.php
@@ -0,0 +1,14 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+<meta name="theme-color" content="#0b0c0c">
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+<link rel="shortcut icon" sizes="16x16 32x32 48x48" href="/wp-content/themes/govuk-theme/static/lib/govuk-frontend/govuk/assets/images/favicon.ico" type="image/x-icon">
+<link rel="mask-icon" href="/assets/images/govuk-mask-icon.svg" color="#0b0c0c">
+<link rel="apple-touch-icon" sizes="180x180" href="/wp-content/themes/govuk-theme/static/lib/govuk-frontend/govuk/assets/images/govuk-apple-touch-icon-180x180.png">
+<link rel="apple-touch-icon" sizes="167x167" href="/wp-content/themes/govuk-theme/static/lib/govuk-frontend/govuk/assets/images/govuk-apple-touch-icon-167x167.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/wp-content/themes/govuk-theme/static/lib/govuk-frontend/govuk/assets/images/govuk-apple-touch-icon-152x152.png">
+<link rel="apple-touch-icon" href="/wp-content/themes/govuk-theme/static/lib/govuk-frontend/govuk/assets/images/govuk-apple-touch-icon.png">
+
+<meta property="og:image" content="/wp-content/themes/govuk-theme/static/lib/govuk-frontend/govuk/assets/images/govuk-opengraph-image.png">
