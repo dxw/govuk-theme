13ccbf5f1f6d0f6bc5d0b450e8cc241032e80606
Rob Skilling <rob@dxw.com>
2022-02-09 14:42:44 +0000

    Remove stray space from placeholder text

--- templates/partials/search-form.php
+++ templates/partials/search-form.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url('/'); ?>">
 
-    <input type="search" value="<?php echo is_search() ? get_search_query() : '' ?>" name="s" id="site-search" class="search-field form-control" placeholder="<?php _e('Search on ', 'govuk-theme'); ?> <?php bloginfo('name'); ?>">
+    <input type="search" value="<?php echo is_search() ? get_search_query() : '' ?>" name="s" id="site-search" class="search-field form-control" placeholder="<?php _e('Search on ', 'govuk-theme'); ?><?php bloginfo('name'); ?>">
 
     <label class="govuk-visually-hidden" for="site-search"><?php _e('Search on ', 'govuk-theme'); ?> <?php bloginfo('name'); ?></label>
 
