944f710e20bf64c4ca7016c560e8ca36b8e5abea
Rob Skilling <rob@dxw.com>
2022-02-10 11:30:06 +0000

    Do not render any breadcrumb HTML when no breadcrumb
    Before: the outlining HTML from the breadcrumb trail would still be
    rendered, even when there was no breadcrumb
    
    Now: now breadcrumb HTML will be rendered when there's no breadcrumb

--- templates/partials/breadcrumb.php
+++ templates/partials/breadcrumb.php
@@ -3,19 +3,18 @@
 global $post;
 
 $ancestors = array_reverse(get_post_ancestors($post->ID)); ?>
-
-<div class="govuk-breadcrumbs ">
-    <ol class="govuk-breadcrumbs__list">
-        <?php if (!is_front_page() && !is_search()) : ?>
-            <li class="govuk-breadcrumbs__list-item">
-                <a class="govuk-breadcrumbs__link" href="<?php echo get_site_url(); ?>"><?php _e('Home', 'govuk-theme') ?></a>
-            </li>
-            <?php foreach ($ancestors as $ancestor) : ?>
+<?php if (!is_front_page() && !is_search()) : ?>
+    <div class="govuk-breadcrumbs ">
+        <ol class="govuk-breadcrumbs__list">
                 <li class="govuk-breadcrumbs__list-item">
-                    <a class="govuk-breadcrumbs__link" href="<?php the_permalink($ancestor) ?>"><?php echo get_the_title($ancestor) ?></a>
+                    <a class="govuk-breadcrumbs__link" href="<?php echo get_site_url(); ?>"><?php _e('Home', 'govuk-theme') ?></a>
                 </li>
-            <?php endforeach; ?>
-            <li class="govuk-breadcrumbs__list-item"><?php the_title() ?></li>
-        <?php endif; ?>
-    </ol>
-</div>
+                <?php foreach ($ancestors as $ancestor) : ?>
+                    <li class="govuk-breadcrumbs__list-item">
+                        <a class="govuk-breadcrumbs__link" href="<?php the_permalink($ancestor) ?>"><?php echo get_the_title($ancestor) ?></a>
+                    </li>
+                <?php endforeach; ?>
+                <li class="govuk-breadcrumbs__list-item"><?php the_title() ?></li>       
+        </ol>
+    </div>
+<?php endif; ?>
