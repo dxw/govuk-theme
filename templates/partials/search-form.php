f91ca317f143361ab7c44dd2a04b1ef028203d5d
Rob Skilling <rob@dxw.com>
2022-02-10 11:05:03 +0000

    Add aria-labelledby attribute for search button text

--- templates/partials/search-form.php
+++ templates/partials/search-form.php
@@ -6,7 +6,8 @@
         <label class="<?php echo apply_filters('govuk_theme_class', 'govuk-visually-hidden'); ?>" for="site-search"><?php _e('Search on ', 'govuk-theme'); ?> <?php bloginfo('name'); ?></label>
 
         <div class="search-form__submit-wrapper">
-            <button class="<?php echo apply_filters('govuk_theme_class', 'govuk-button search-form__button') ?>" data-module="govuk-button"><?php _e('Search', 'govuk-theme'); ?>
+            <button class="<?php echo apply_filters('govuk_theme_class', 'govuk-button search-form__button') ?>" data-module="govuk-button" aria-labelledby="search-form__button-label">
+                <span id="search-form__button-label" hidden=""><?php _e('Search', 'govuk-theme'); ?></span>
                 <svg class="search-form__icon" width="27" height="27" viewBox="0 0 27 27" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">
                     <circle cx="12.0161" cy="11.0161" r="8.51613" stroke="currentColor" stroke-width="3"></circle>
                     <line x1="17.8668" y1="17.3587" x2="26.4475" y2="25.9393" stroke="currentColor" stroke-width="3"></line>
