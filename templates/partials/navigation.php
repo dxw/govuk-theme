79197673f51c33cfb54fd81896bebab18e0a8cb4
Patrick Delaney <patrick.delaney@H26H02022Y.local>
2025-06-11 14:56:11 +0100

    Add `hidden` attribute to menu button
    Some changes were made to the styles of govuk-header__menu-button. To
    ensure it continues to work as expected, the `hidden` attribute was
    added.
    
    https://github.com/alphagov/govuk-frontend/blob/main/CHANGELOG.md#add-the-hidden-attribute-to-the-mobile-menu-button-in-the-header-component

--- templates/partials/navigation.php
+++ templates/partials/navigation.php
@@ -1,5 +1,5 @@
 <nav class="header-navigation" role="navigation">
-    <button type="button" class="<?php echo apply_filters('govuk_theme_class', 'govuk-header__menu-button govuk-js-header-toggle') ?>" aria-controls="header-navigation" aria-label="<?php _e('Show or hide Top Level Navigation', 'govuk-theme') ?>"><?php _e('Menu', 'govuk-theme') ?></button>
+    <button type="button" class="<?php echo apply_filters('govuk_theme_class', 'govuk-header__menu-button govuk-js-header-toggle') ?>" aria-controls="header-navigation" aria-label="<?php _e('Show or hide Top Level Navigation', 'govuk-theme') ?>" hidden><?php _e('Menu', 'govuk-theme') ?></button>
     <?php
     if (has_nav_menu('header')) {
         wp_nav_menu(array(
